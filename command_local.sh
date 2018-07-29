059d08b46984b9d93a749ca8366e7ace9c0290b1
iarroyof <axayakac@gmail.com>
2018-07-28 21:11:23 -0500

    results added for STS 2016

--- command_local.sh
+++ command_local.sh
@@ -2,9 +2,10 @@
 # STS benchmark and sick
 #parallel 'in=$(echo {5}|cut -d'.' -f 2); python sts.py --stop {3} --format wisse --idfmodel local --tfidf {1} --localw {2} --embedmodel {4} --input {5} --comb {6} --dist {7} --output results_revision/local-$(echo {1}-{2}-{3}-{4/}-${in}-{6}-{7}.out | sed "s/--/-wst-/g")' ::: `cat tfidf.l` ::: `cat localw.l` ::: `cat stop.l` ::: `cat embedmodel.l` ::: `cat input.l` ::: `cat comb.l` ::: `cat dist.l`
 # STS benchmark unweighted
-parallel 'in=$(echo {3}|cut -d'.' -f 2); python sts.py --stop {1} --format wisse --idfmodel none --tfidf none --localw none --embedmodel {2} --input {3} --comb {4} --dist {5} --output results_revision/unweighted-$(echo {1}-{2/}-${in}-{4}-{5}.out | sed "s/--/-wst-/g")' ::: `cat stop.l` ::: `cat embedmodel.l` ::: `cat input.l` ::: `cat comb.l` ::: `cat dist.l`
+#parallel 'in=$(echo {3}|cut -d'.' -f 2); python sts.py --stop {1} --format wisse --idfmodel none --tfidf none --localw none --embedmodel {2} --input {3} --comb {4} --dist {5} --output results_revision/unweighted-$(echo {1}-{2/}-${in}-{4}-{5}.out | sed "s/--/-wst-/g")' ::: `cat stop.l` ::: `cat embedmodel.l` ::: `cat input.l` ::: `cat comb.l` ::: `cat dist.l`
 # STS 2016
 #parallel 'in=$(echo {5}|cut -d'.' -f 4); python sts.py --stop {3} --format wisse --idfmodel local --tfidf {1} --localw {2} --embedmodel {4} --input {5} --comb {6} --dist {7} --output results_revision/local-$(echo {1}-{2}-{3}-{4/}-${in}-{6}-{7}.out | sed "s/--/-wst-/g")' ::: `cat tfidf.l` ::: `cat localw.l` ::: `cat stop.l` ::: `cat embedmodel.l` ::: `cat _input.l` ::: `cat comb.l` ::: `cat dist.l`
 # STS 2016 unweighted
-parallel 'in=$(echo {3}|cut -d'.' -f 4); python sts.py --stop {1} --format wisse --idfmodel none --tfidf none --localw none --embedmodel {2} --input {3} --comb {4} --dist {5} --output results_revision/unweighted$(echo -{1}-{2/}-${in}-{4}-{5}.out | sed "s/--/-wst-/g")' ::: `cat stop.l` ::: `cat embedmodel.l` ::: `cat _input.l` ::: `cat comb.l` ::: `cat dist.l`
-
+#parallel 'in=$(echo {3}|cut -d'.' -f 4); python sts.py --stop {1} --format wisse --idfmodel none --tfidf none --localw none --embedmodel {2} --input {3} --comb {4} --dist {5} --output results_revision/unweighted$(echo -{1}-{2/}-${in}-{4}-{5}.out | sed "s/--/-wst-/g")' ::: `cat stop.l` ::: `cat embedmodel.l` ::: `cat _input.l` ::: `cat comb.l` ::: `cat dist.l`
+# STS 2016 final
+parallel 'in=$(echo {3}|cut -d'.' -f 4|sed 's/-/_/g'); python3 sts.py --stop {2} --format wisse --idfmodel local --tfidf tfidf --localw {1} --embedmodel links/fstx-300d --input {3} --comb sum --dist cosine --output results_revision/local-$(echo tfidf-{1}-{2}-fstx-300d-${in}-sum-cosine.out | sed "s/--/-wst-/g")' ::: `cat _localw.l` ::: `cat stop.l` ::: `cat _input.l`
--- tabulate_results.py
+++ tabulate_results.py
@@ -7,7 +7,7 @@ import sys
 printfile = True
 results = "results_revision/"
 #DATA = "/almac/ignacio/data"
-DATA = "../data"
+DATA = "../data/sts_all"
 out = "out_tab.csv"
 # cat out_tab.csv | parallel -- > out_all.out
 
@@ -26,32 +26,32 @@ with open(out, "w") as of:
     for j in filtered:
         f = j.split("-")[6]
         if f == "bmk_dev" and not "bmk" in filters: # and not "dev" in filters:
-            gs = DATA + "/sts/benchmark_2017/sts-gs.dev.txt"
+            gs = DATA + "/benchmark_2017/sts-gs.dev.txt"
         elif f == "bmk_train" and not "bmk" in filters: # and not "train" in filters:
-            gs = DATA + "/sts/benchmark_2017/sts-gs.train.txt"
+            gs = DATA + "/benchmark_2017/sts-gs.train.txt"
         elif f == "bmk_test" and not "bmk" in filters: # and not "test" in filters:
-            gs = DATA + "/sts/benchmark_2017/sts-gs.test.txt"
+            gs = DATA + "/benchmark_2017/sts-gs.test.txt"
         elif f == "sick_trial" and not "sick" in filters: # and not "trial" in filters:
-            gs = DATA + "/sts/sick/sts.gs.sick-trial.txt"
+            gs = DATA + "/sick/sts.gs.sick-trial.txt"
         elif f == "sick_trts" and not "sick" in filters: # and not "trts" in filters:
-            gs = DATA + "/sts/sick/sts.gs.zick.txt"
+            gs = DATA + "/sick/sts.gs.zick.txt"
         elif f == "answer-answer" and not "answer" in filters: # and not "trts" in filters:
-            gs = DATA + "/sts_all/sts2016-english-with-gs-v1.0/STS2016.gs.answer-answer.txt"
+            gs = DATA + "/sts2016-english-with-gs-v1.0/STS2016.gs.answer-answer.txt"
         elif f == "headlines" and not "headlines" in filters: # and not "trts" in filters:
-            gs = DATA + "/sts_all/sts2016-english-with-gs-v1.0/STS2016.gs.headlines.txt"
+            gs = DATA + "/sts2016-english-with-gs-v1.0/STS2016.gs.headlines.txt"
         elif f == "plagiarism" and not "plagiarism" in filters: # and not "trts" in filters:
-            gs = DATA + "/sts_all/sts2016-english-with-gs-v1.0/STS2016.gs.plagiarism.txt"
+            gs = DATA + "/sts2016-english-with-gs-v1.0/STS2016.gs.plagiarism.txt"
         elif f == "postediting" and not "postediting" in filters: # and not "trts" in filters:
-            gs = DATA + "/sts_all/sts2016-english-with-gs-v1.0/STS2016.gs.postediting.txt"
+            gs = DATA + "/sts2016-english-with-gs-v1.0/STS2016.gs.postediting.txt"
         elif f == "question-question" and not "question" in filters: # and not "trts" in filters:
-            gs = DATA + "/sts_all/sts2016-english-with-gs-v1.0/STS2016.gs.question-question.txt"
+            gs = DATA + "/sts2016-english-with-gs-v1.0/STS2016.gs.question-question.txt"
         else:
             gs = ""
 
         if gs != "":
-            command=("gs={0}; sys={1}; if [[ $(wc -l $gs | cut -d' ' -f1) = $(wc -l $sys | cut -d' ' -f1)  ]];then"
+            command=("gs={0}; sys={1};" #if [[ $(wc -l $gs | cut -d' ' -f1) = $(wc -l $sys | cut -d' ' -f1)  ]];then"
                     " p=$(perl correlation-noconfidence.pl $gs $sys | cut -d' ' -f2); name=$(echo $(basename $sys) | "
-                    "sed 's/-/ /g'); echo ${{name%.out}} ${{p//-}}; fi\n")
+                    "sed 's/-/ /g'); echo ${{name%.out}} ${{p//-}};\n") #fi\n")
             if printfile:
                 of.write (command.format(gs, results + j ))
             else:
